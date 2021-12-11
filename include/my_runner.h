f6771b66fe6ec8f60fb81ffeafb2ced1f2b9d8ca
saverio <xavier.mitault@epitech.eu>
2021-12-11 01:03:59 +0100

    [add] include/my_runner.h: function prototype of the menu

--- include/my_runner.h
+++ include/my_runner.h
@@ -13,11 +13,16 @@
 // MACRO
 
     #define INTRO_BG_PATH "assets/background_intro.png"
+    #define MENU_MUSIC_PATH "assets/forever_bound_stereo_madness.flac"
+    #define FONT_PATH "assets/fonts.otf"
+    #define PLAY_BUTTON_PATH "assets/play.png"
 
 typedef struct game_runner game_runner_t;
 
 struct game_runner {
     int life;
+    sfMusic *music;
+    int is_playing;
 };
 
 // ****************************************************************************
@@ -43,6 +48,23 @@ void s_destroy_intro(scenne_entity_t *, window_controler_t *);
 // OBJ
 
 int o_update_intro_background(object_entity_t *, scenne_entity_t *,
+        window_controler_t *);
+
+// *****************
+// MENU
+
+int s_create_menu(scenne_entity_t *, window_controler_t *);
+
+int s_update_menu(scenne_entity_t *, window_controler_t *);
+
+void s_destroy_menu(scenne_entity_t *, window_controler_t *);
+
+// OBJ
+
+int o_update_menu_settings(object_entity_t *, scenne_entity_t *,
+        window_controler_t *);
+
+int o_update_menu_play(object_entity_t *, scenne_entity_t *,
         window_controler_t *);
 
 #endif
