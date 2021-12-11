102426236dac05360861cafaf5ffee7714867544
saverio <xavier.mitault@epitech.eu>
2021-12-11 20:13:14 +0100

    [add] src/main.c: a settings scene

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 /*
 ** EPITECH PROJECT, 2021
-** ...
+** MYRUNNNER
 ** File description:
 ** the main entry point for the project
 */
@@ -12,9 +12,11 @@
 int main(void)
 {
     sfVideoMode mode = {800, 600, 32};
-    window_controler_t *manager = create(&w_create_runner, &w_destroy_runner);
+    window_controler_t *manager = create(w_create_runner, w_destroy_runner);
 
-    create_scenne(manager, &s_create_intro, &s_update_intro, &s_destroy_intro);
-    create_scenne(manager, &s_create_menu, &s_update_menu, &s_destroy_menu);
+    create_scenne(manager, s_create_intro, s_update_intro, s_destroy_intro);
+    create_scenne(manager, s_create_menu, s_update_menu, s_destroy_menu);
+    create_scenne(manager, s_create_settings, s_update_settings,
+            s_destroy_settings);
     return (start(manager, "Geometry Dash", mode, sfResize));
 }
