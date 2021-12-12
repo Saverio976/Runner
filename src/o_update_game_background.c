bc36b46556b7c1fe356ee758e1ea79d94625bb24
saverio <xavier.mitault@epitech.eu>
2021-12-12 01:03:36 +0100

    [add] src/o_update_game_background.c: update the beackground of the game scene

---
+++ src/o_update_game_background.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2021
+** MYRUNNER
+** File description:
+** function to update the background
+*/
+
+#include "my_gras.h"
+#include "my_runner.h"
+
+int o_update_game_background(object_entity_t *obj,
+        __attribute__((unused)) scenne_entity_t *scene,
+        window_controler_t *manager)
+{
+    sfRenderWindow_drawSprite(manager->win, obj->sprite, NULL);
+    return (0);
+}
