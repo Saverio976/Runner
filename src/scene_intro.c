217553b72a99fd59fe29d3b7d62079fb65783b7e
saverio <xavier.mitault@epitech.eu>
2021-12-13 11:11:57 +0100

    [update] src/o_update_intro_background.c src/scene_intro.c: add the function for o_update_intro_background to scene_intro.c

--- src/scene_intro.c
+++ src/scene_intro.c
@@ -9,6 +9,14 @@
 #include "my_gras.h"
 #include "my_runner.h"
 
+int o_update_intro_background(object_entity_t *obj,
+        __attribute__((unused)) scenne_entity_t *scene,
+        window_controler_t *manager)
+{
+    sfRenderWindow_drawSprite(manager->win, obj->sprite, NULL);
+    return (0);
+}
+
 void s_destroy_intro(scenne_entity_t *scenne,
         __attribute__((unused)) window_controler_t *manager)
 {
