4cee65226f6934922fdc694d2e77e3a54186b12c
saverio <xavier.mitault@epitech.eu>
2021-12-13 11:20:10 +0100

    [update] include/my_runner.h: update controler to controller and scenne to scene

--- include/my_runner.h
+++ include/my_runner.h
@@ -51,97 +51,97 @@ struct game_player {
 // utility
 // ****************************************************************************
 
-int create_map_obj_from(game_runner_t *, scenne_entity_t *);
+int create_map_obj_from(game_runner_t *, scene_entity_t *);
 
-int pass_game_to_menu(window_controler_t *);
+int pass_game_to_menu(window_controller_t *);
 
-int restart_game(scenne_entity_t *, window_controler_t *);
+int restart_game(scene_entity_t *, window_controller_t *);
 
 int check_point_in(float, float, sfFloatRect *);
 
 // ****************************************************************************
-// WINDOW_CONTROLER
+// WINDOW_controller
 // ****************************************************************************
 
-int w_create_runner(window_controler_t *);
+int w_create_runner(window_controller_t *);
 
-void w_destroy_runner(window_controler_t *);
+void w_destroy_runner(window_controller_t *);
 
 // ***********************************
-// SCENNES
+// sceneS
 
 // *****************
 // INTRO
 
-int s_create_intro(scenne_entity_t *, window_controler_t *);
+int s_create_intro(scene_entity_t *, window_controller_t *);
 
-int s_update_intro(scenne_entity_t *, window_controler_t *);
+int s_update_intro(scene_entity_t *, window_controller_t *);
 
-void s_destroy_intro(scenne_entity_t *, window_controler_t *);
+void s_destroy_intro(scene_entity_t *, window_controller_t *);
 
 // OBJ
 
-int o_update_intro_background(object_entity_t *, scenne_entity_t *,
-        window_controler_t *);
+int o_update_intro_background(object_entity_t *, scene_entity_t *,
+        window_controller_t *);
 
 // *****************
 // MENU
 
-int s_create_menu(scenne_entity_t *, window_controler_t *);
+int s_create_menu(scene_entity_t *, window_controller_t *);
 
-int s_update_menu(scenne_entity_t *, window_controler_t *);
+int s_update_menu(scene_entity_t *, window_controller_t *);
 
-void s_destroy_menu(scenne_entity_t *, window_controler_t *);
+void s_destroy_menu(scene_entity_t *, window_controller_t *);
 
 // OBJ
 
-int o_update_menu_settings(object_entity_t *, scenne_entity_t *,
-        window_controler_t *);
+int o_update_menu_settings(object_entity_t *, scene_entity_t *,
+        window_controller_t *);
 
-int o_update_menu_play(object_entity_t *, scenne_entity_t *,
-        window_controler_t *);
+int o_update_menu_play(object_entity_t *, scene_entity_t *,
+        window_controller_t *);
 
 // *****************
 // SETTINGS
 
-int s_create_settings(scenne_entity_t *, window_controler_t *);
+int s_create_settings(scene_entity_t *, window_controller_t *);
 
-int s_update_settings(scenne_entity_t *, window_controler_t *);
+int s_update_settings(scene_entity_t *, window_controller_t *);
 
-void s_destroy_settings(scenne_entity_t *, window_controler_t *);
+void s_destroy_settings(scene_entity_t *, window_controller_t *);
 
 // OBJ
 
-int o_update_settings_music(object_entity_t *, scenne_entity_t *,
-        window_controler_t *);
+int o_update_settings_music(object_entity_t *, scene_entity_t *,
+        window_controller_t *);
 
-int o_update_settings_menu(object_entity_t *, scenne_entity_t *,
-        window_controler_t *);
+int o_update_settings_menu(object_entity_t *, scene_entity_t *,
+        window_controller_t *);
 
-int o_update_settings_switch(object_entity_t *, scenne_entity_t *,
-        window_controler_t *);
+int o_update_settings_switch(object_entity_t *, scene_entity_t *,
+        window_controller_t *);
 
 // *****************
 // GAME
 
-int s_create_game(scenne_entity_t *, window_controler_t *);
+int s_create_game(scene_entity_t *, window_controller_t *);
 
-int s_update_game(scenne_entity_t *, window_controler_t *);
+int s_update_game(scene_entity_t *, window_controller_t *);
 
-void s_destroy_game(scenne_entity_t *, window_controler_t *);
+void s_destroy_game(scene_entity_t *, window_controller_t *);
 
 // OBJ
 
-int o_update_game_background(object_entity_t *, scenne_entity_t *,
-        window_controler_t *);
+int o_update_game_background(object_entity_t *, scene_entity_t *,
+        window_controller_t *);
 
-int o_update_game_block(object_entity_t *, scenne_entity_t *,
-        window_controler_t *);
+int o_update_game_block(object_entity_t *, scene_entity_t *,
+        window_controller_t *);
 
-int o_update_game_time(object_entity_t *, scenne_entity_t *,
-        window_controler_t *);
+int o_update_game_time(object_entity_t *, scene_entity_t *,
+        window_controller_t *);
 
-int o_update_game_paralax_2(object_entity_t *, scenne_entity_t *,
-        window_controler_t *);
+int o_update_game_paralax_2(object_entity_t *, scene_entity_t *,
+        window_controller_t *);
 
 #endif
