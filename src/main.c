41c28e93ab3c3777ac7866d35b53092b3f16fb67
Saverio <xavier.mitault@epitech.eu>
2021-12-15 18:04:25 +0100

    [fix] src/main.c: add check the return type

--- src/main.c
+++ src/main.c
@@ -14,6 +14,8 @@ int main(void)
     sfVideoMode mode = {800, 600, 32};
     window_controller_t *manager = create(w_create_runner, w_destroy_runner);
 
+    if (manager == NULL)
+        return (84);
     create_scene(manager, s_create_intro, s_update_intro, s_destroy_intro);
     create_scene(manager, s_create_menu, s_update_menu, s_destroy_menu);
     create_scene(manager, s_create_settings, s_update_settings,
