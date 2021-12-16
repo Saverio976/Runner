f8787468334f34a425827ed10cf514b4e40ebc4c
Saverio <xavier.mitault@epitech.eu>
2021-12-16 23:51:32 +0100

    [add] src/main.c: possibility to add a -f for ful screen

--- src/main.c
+++ src/main.c
@@ -10,8 +10,6 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-
-
 static int check_file_path(char **av)
 {
     int fd = open(av[1], O_RDONLY);
@@ -22,25 +20,39 @@ static int check_file_path(char **av)
     return (1);
 }
 
-int main(int ac, char **av)
+static int parse_args(int ac, char **av, int *fullscreen)
 {
-    sfVideoMode mode = {800, 600, 32};
-    window_controller_t *manager;
-
     if (ac < 2)
         return (print_error(av[0], ": need one args; retry with -h\n"));
     if (len_calc(av[1]) == 2 && av[1][0] == '-' && av[1][1] == 'h')
         return (print_help(av[0]));
     if (check_file_path(av) == 0)
         return (print_error(av[0], ": provide a map available (map/42.txt)\n"));
+    if (ac > 2 && len_calc(av[2]) == 2 && av[2][0] == '-' && av[2][1] == 'f')
+        *fullscreen = 1;
+    return (1);
+}
+
+int main(int ac, char **av)
+{
+    sfVideoMode mode = {800, 600, 32};
+    window_controller_t *manager;
+    int fullscreen = 0;
+    sfUint32 style = 0;
+    int ret_code = parse_args(ac, av, &fullscreen);
+
+    if (ret_code != 1)
+        return (ret_code);
     manager = create(w_create_runner, w_destroy_runner);
     if (manager == NULL)
         return (84);
+    if (fullscreen)
+        style = style | sfFullscreen;
     ((game_runner_t *) manager->data)->settings.map_path = av[1];
     create_scene(manager, s_create_intro, s_update_intro, s_destroy_intro);
     create_scene(manager, s_create_menu, s_update_menu, s_destroy_menu);
     create_scene(manager, s_create_settings, s_update_settings,
             s_destroy_settings);
     create_scene(manager, s_create_game, s_update_game, s_destroy_game);
-    return (start(manager, "Geometry Dash", mode, sfResize));
+    return (start(manager, "Geometry Dash", mode, style));
 }
