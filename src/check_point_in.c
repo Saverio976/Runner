86c26cfe79adb7c0fdb8eb092da266167c665a82
Saverio <xavier.mitault@epitech.eu>
2021-12-16 23:55:25 +0100

    [add] src/check_point_in.c: add  function to check collision

--- src/check_point_in.c
+++ src/check_point_in.c
@@ -5,7 +5,21 @@
 ** check if a point is in a suqare
 */
 
-#include <SFML/Graphics.h>
+#include "my_gras.h"
+#include "my_runner.h"
+
+int collision_between(game_player_t *player, object_entity_t *obj)
+{
+    sfFloatRect bounds_pl = sfSprite_getGlobalBounds(player->sprite);
+    sfFloatRect bounds_ob = sfSprite_getGlobalBounds(obj->sprite);
+
+    if (bounds_ob.top + bounds_ob.height < bounds_pl.top ||
+            bounds_ob.top > bounds_pl.top + bounds_pl.height ||
+            bounds_ob.left + bounds_ob.width < bounds_pl.left ||
+            bounds_ob.left > bounds_pl.left + bounds_pl.width)
+        return (0);
+    return (1);
+}
 
 int check_point_in(float px, float py, sfFloatRect *rect)
 {
