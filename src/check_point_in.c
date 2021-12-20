bd8462b2ee7ef3196540eb06b4e0c152e8b4ae74
Saverio <xavier.mitault@epitech.eu>
2021-12-20 23:45:55 +0100

    [update] src/check_point_in.c: use a function that provide the csfml for collision

--- src/check_point_in.c
+++ src/check_point_in.c
@@ -13,12 +13,9 @@ int collision_between(game_player_t *player, object_entity_t *obj)
     sfFloatRect bounds_pl = sfSprite_getGlobalBounds(player->sprite);
     sfFloatRect bounds_ob = sfSprite_getGlobalBounds(obj->sprite);
 
-    if (bounds_ob.top + bounds_ob.height < bounds_pl.top ||
-            bounds_ob.top > bounds_pl.top + bounds_pl.height ||
-            bounds_ob.left + bounds_ob.width < bounds_pl.left ||
-            bounds_ob.left > bounds_pl.left + bounds_pl.width)
-        return (0);
-    return (1);
+    if (sfFloatRect_intersects(&bounds_pl, &bounds_ob, NULL))
+        return (1);
+    return (0);
 }
 
 int check_point_in(float px, float py, sfFloatRect *rect)
