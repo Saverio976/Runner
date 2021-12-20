e67d84de65012263561b464bfe670dc892e8ffbf
Saverio <xavier.mitault@epitech.eu>
2021-12-21 00:13:25 +0100

    [update] src/check_point_in.c: better collision between the icon and triangle

--- src/check_point_in.c
+++ src/check_point_in.c
@@ -12,9 +12,18 @@ int collision_between(game_player_t *player, object_entity_t *obj)
 {
     sfFloatRect bounds_pl = sfSprite_getGlobalBounds(player->sprite);
     sfFloatRect bounds_ob = sfSprite_getGlobalBounds(obj->sprite);
+    float tmp = bounds_ob.height / 5;
 
-    if (sfFloatRect_intersects(&bounds_pl, &bounds_ob, NULL))
-        return (1);
+    if (obj->data != (void *) 115)
+        return (sfFloatRect_intersects(&bounds_pl, &bounds_ob, NULL));
+    bounds_ob.top += bounds_ob.height;
+    for (int i = 5; i > 0; i--) {
+        bounds_ob.top -= tmp;
+        bounds_ob.left += bounds_ob.width / 3;
+        bounds_ob.width = (bounds_ob.width / 3) * 2;
+        if (sfFloatRect_intersects(&bounds_pl, &bounds_ob, NULL))
+            return (1);
+    }
     return (0);
 }
 
