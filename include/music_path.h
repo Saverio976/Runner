0e2e8d5fa8bddbe7d57555338df782cb4ce4ba3b
Saverio <xavier.mitault@epitech.eu>
2021-12-21 21:00:22 +0100

    [update] include/music_path.h: update the macro containing the numer max music

--- include/music_path.h
+++ include/music_path.h
@@ -9,13 +9,14 @@
     #define MUSICS_H_
 
     #include <stddef.h>
-    #define NUMBER_MUSICS 4
+    #define NUMBER_MUSICS 5
 
 static const char *MUSICS[] = {
     "assets/music/djvi_base_after_base.ogg",
     "assets/music/forever_bound_stereo_madness.ogg",
     "assets/music/djvi_cant_let_go.ogg",
     "assets/music/waterflame_jumper.ogg",
+    "assets/music/vicetone_tonyigy_astronomia.ogg",
     NULL
 };
 
